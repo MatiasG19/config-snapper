9cb58d27aae93bdd68db1cdcc1a798485358ab98
MatiasG19 <m.galusek@gmail.com>
2025-06-11 18:26:13 +0200

    add console app

---
+++ src/ConfigSnapperConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using Matiasg19.ConfigSnapper;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddConfigSnapper(builder.Configuration);
+
+var host = builder.Build();
+
+host.Services.UseConfigSnapper(builder.Configuration);
+
+host.Start();
