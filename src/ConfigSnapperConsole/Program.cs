10d773427e7b40c601f566d518ceca4c1bd54bf0
MatiasG19 <m.galusek@gmail.com>
2025-07-06 00:07:32 +0200

    add serilog

--- src/ConfigSnapperConsole/Program.cs
+++ src/ConfigSnapperConsole/Program.cs
@@ -1,7 +1,14 @@
 ﻿using Matiasg19.ConfigSnapper;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSerilog(config =>
+{
+    config.ReadFrom.Configuration(builder.Configuration);
+});
+
 
 string[] arguments = Environment.GetCommandLineArgs();
 builder.Services.AddConfigSnapper(arguments.Length > 1 ? arguments[1] : null);
