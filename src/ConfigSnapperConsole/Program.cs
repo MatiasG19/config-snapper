767c1812892ebc3d471200a0ac10ec74061f84ff
MatiasG19 <m.galusek@gmail.com>
2025-07-10 20:40:34 +0200

    format

--- src/ConfigSnapperConsole/Program.cs
+++ src/ConfigSnapperConsole/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddSerilog(config =>
     config.ReadFrom.Configuration(builder.Configuration);
 });
 
-
 string[] arguments = Environment.GetCommandLineArgs();
 builder.Services.AddConfigSnapper(arguments.Length > 1 ? arguments[1] : null);
 
