e1ec0976cd0f63c85c8a41e9626901febe26ec3c
MatiasG19 <m.galusek@gmail.com>
2025-07-13 01:46:07 +0200

    fix config reading

--- src/ConfigSnapperConsole/Program.cs
+++ src/ConfigSnapperConsole/Program.cs
@@ -1,14 +1,30 @@
-﻿using Matiasg19.ConfigSnapper;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Matiasg19.ConfigSnapper;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Sinks.SystemConsole;
+using Serilog.Sinks.File;
+using OpenTelemetry;
+using Microsoft.Extensions.DependencyInjection;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
+string[] arguments = Environment.GetCommandLineArgs();
+const string appSettings = "appSettings.json";
+string appSettingsPath = arguments.Length > 1 ?
+    Path.Combine(arguments[1], appSettings) :
+    Path.Combine(Directory.GetCurrentDirectory(), appSettings);
+
 var configFile = new ConfigurationBuilder()
-    .AddJsonFile("appSettings.json", optional: true, reloadOnChange: false)
+    .AddJsonFile(appSettingsPath, optional: false, reloadOnChange: false)
     .Build();
+
+Matiasg19.ConfigSnapper.Configuration.ConfigSnapper snapperConfig = new ();
+configFile.GetSection(nameof(Matiasg19.ConfigSnapper.Configuration.ConfigSnapper)).Bind(snapperConfig);
+
+builder.Services.AddOptions<Matiasg19.ConfigSnapper.Configuration.ConfigSnapper>()
+    .Bind(configFile.GetSection(nameof(Matiasg19.ConfigSnapper.Configuration.ConfigSnapper)));
 
 builder.Logging.ClearProviders();
 builder.Services.AddSerilog(config =>
@@ -16,11 +32,20 @@ builder.Services.AddSerilog(config =>
     config.ReadFrom.Configuration(configFile);
 });
 
-string[] arguments = Environment.GetCommandLineArgs();
-builder.Services.AddConfigSnapper(arguments.Length > 1 ? arguments[1] : null);
+if (snapperConfig.OpenTelemetry)
+{
+    builder.Services.AddLogging(builder => builder.AddOpenTelemetry());
+    var otel = builder.Services.AddOpenTelemetry();
+    if (configFile["OTEL_EXPORTER_OTLP_ENDPOINT"] != null)
+    {
+        otel.UseOtlpExporter();
+    }
+}
+
+builder.Services.AddConfigSnapper();
 
 var host = builder.Build();
 
 host.Start();
 
-host.Services.UseConfigSnapper(builder.Configuration);
+host.Services.UseConfigSnapper();
