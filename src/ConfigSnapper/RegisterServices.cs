b1b61c0ff34094abf6393ae9c2fe5f752aa235be
MatiasG19 <73800355+MatiasG19@users.noreply.github.com>
2025-07-13 09:40:29 +0200

    fix: config reading, move otel to console app (#4)
    * fix config reading
    
    * format
    
    * Update packages.lock.json
    
    * remove unused dependencies

--- src/ConfigSnapper/RegisterServices.cs
+++ src/ConfigSnapper/RegisterServices.cs
@@ -1,49 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using OpenTelemetry;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace Matiasg19.ConfigSnapper;
 
 public static class RegisterServices
 {
-    private static string appSettings = "appSettings.json";
-
-    public static void AddConfigSnapper(this IServiceCollection services, string? pathToAppSettings = null)
+    public static void AddConfigSnapper(this IServiceCollection services)
     {
-        Configuration.ConfigSnapper snapperConfig = new Configuration.ConfigSnapper();
-
-        string appSettingsPath = pathToAppSettings is not null ?
-            Path.Combine(pathToAppSettings, appSettings) :
-            appSettings;
-
-        var configuration = new ConfigurationBuilder()
-            .AddJsonFile(appSettingsPath, optional: false, reloadOnChange: true)
-            .Build();
-
-        configuration.GetSection(nameof(Configuration.ConfigSnapper)).Bind(snapperConfig);
-
-        services.AddOptions<Configuration.ConfigSnapper>()
-            .Bind(configuration.GetSection(nameof(Configuration.ConfigSnapper)));
-
-        services.AddLogging(builder => builder.AddConsole());
-
-        if (snapperConfig.OpenTelemetry)
-        {
-            services.AddLogging(builder => builder.AddOpenTelemetry());
-            var otel = services.AddOpenTelemetry();
-            if (configuration["OTEL_EXPORTER_OTLP_ENDPOINT"] != null)
-            {
-                otel.UseOtlpExporter();
-            }
-        }
-
         services.AddSingleton<Snapper>();
-
         Console.WriteLine("Service registered: ConfigSnapper");
     }
 
-    public static void UseConfigSnapper(this IServiceProvider serviceCollection, IConfiguration configuration)
+    public static void UseConfigSnapper(this IServiceProvider serviceCollection)
     {
         serviceCollection.GetRequiredService<Snapper>().CreateSnapshot();
     }
--- src/ConfigSnapperConsole/Program.cs
+++ src/ConfigSnapperConsole/Program.cs
@@ -1,14 +1,28 @@
 ﻿using Matiasg19.ConfigSnapper;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry;
 using Serilog;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
+string[] arguments = Environment.GetCommandLineArgs();
+const string appSettings = "appSettings.json";
+string appSettingsPath = arguments.Length > 1 ?
+    Path.Combine(arguments[1], appSettings) :
+    Path.Combine(Directory.GetCurrentDirectory(), appSettings);
+
 var configFile = new ConfigurationBuilder()
-    .AddJsonFile("appSettings.json", optional: true, reloadOnChange: false)
+    .AddJsonFile(appSettingsPath, optional: false, reloadOnChange: false)
     .Build();
+
+Matiasg19.ConfigSnapper.Configuration.ConfigSnapper snapperConfig = new();
+configFile.GetSection(nameof(Matiasg19.ConfigSnapper.Configuration.ConfigSnapper)).Bind(snapperConfig);
+
+builder.Services.AddOptions<Matiasg19.ConfigSnapper.Configuration.ConfigSnapper>()
+    .Bind(configFile.GetSection(nameof(Matiasg19.ConfigSnapper.Configuration.ConfigSnapper)));
 
 builder.Logging.ClearProviders();
 builder.Services.AddSerilog(config =>
@@ -16,11 +30,20 @@ builder.Services.AddSerilog(config =>
     config.ReadFrom.Configuration(configFile);
 });
 
-string[] arguments = Environment.GetCommandLineArgs();
-builder.Services.AddConfigSnapper(arguments.Length > 1 ? arguments[1] : null);
+if (snapperConfig.OpenTelemetry)
+{
+    builder.Services.AddLogging(builder => builder.AddOpenTelemetry());
+    var otel = builder.Services.AddOpenTelemetry();
+    if (configFile["OTEL_EXPORTER_OTLP_ENDPOINT"] != null)
+    {
+        otel.UseOtlpExporter();
+    }
+}
+
+builder.Services.AddConfigSnapper();
 
 var host = builder.Build();
 
 host.Start();
 
-host.Services.UseConfigSnapper(builder.Configuration);
+host.Services.UseConfigSnapper();
